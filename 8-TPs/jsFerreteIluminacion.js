2d6f652c357f31a9901d77b3aeddc539e50dbd5d
Santiago Silveira <36273222+SSilveira13@users.noreply.github.com>
2018-03-02 15:36:53 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -8,7 +8,116 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
- 	
+function CalcularPrecio() {
+    var cantidad = parseInt(document.getElementById("Cantidad").value);
+    var lampara = 35;
+    var marca = document.getElementById("Marca").value;
+    var conIva;
+    var impuesto;
+    var final;
+    switch (cantidad) {
+        case 0:
+            document.getElementById("precioDescuento").value = "No esta comprando ninguna lampara";
+            break;
+        case 1:
+            document.getElementById("precioDescuento").value = "El precio final es de $35";
+            break;
+        case 2:
+            document.getElementById("precioDescuento").value = "El precio final es de $70";
+            break;
+        case 3:
+            if (marca == "ArgentinaLuz") {
+                conIva = lampara * cantidad * 1.15;
+                if (conIva < 120) {
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva;
+                }
+                else {
+                    impuesto = conIva * 0.10;
+                    final = conIva + impuesto;
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva + " siendo que pago $" + impuesto + " de IIBB.";
+                }
+            }
+            else if (marca == "FelipeLamparas") {
+                conIva = lampara * cantidad * 1.10;
+                if (conIva < 120) {
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva;
+                }
+                else {
+                    impuesto = conIva * 0.10;
+                    final = conIva + impuesto;
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva + " siendo que pago $" + impuesto + " de IIBB.";
+                }
+            }
+            else {
+                conIva = lampara * cantidad * 1.05;
+                if (conIva < 120) {
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva;
+                }
+                else {
+                    impuesto = conIva * 0.10;
+                    final = conIva + impuesto;
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva + " siendo que pago $" + impuesto + " de IIBB.";
+                }
+            }
+            break;
+        case 4:
+            if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
+                conIva = lampara * cantidad * 1.25;
+                if (conIva < 120) {
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva;
+                }
+                else {
+                    impuesto = conIva * 0.10;
+                    final = conIva + impuesto;
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva + " siendo que pago $" + impuesto + " de IIBB.";
+                }
+            }
+            else {
+                conIva = lampara * cantidad * 1.20;
+                if (conIva < 120) {
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva;
+                }
+                else {
+                    impuesto = conIva * 0.10;
+                    final = conIva + impuesto;
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva + " siendo que pago $" + impuesto + " de IIBB.";
+                }
+            }
+            break;
+        case 5:
+            if (marca == "ArgentinaLuz") {
+                conIva = lampara * cantidad * 1.40;
+                if (conIva < 120) {
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva;
+                }
+                else {
+                    impuesto = conIva * 0.10;
+                    final = conIva + impuesto;
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva + " siendo que pago $" + impuesto + " de IIBB.";
+                }
+            }
+            else {
+                conIva = lampara * cantidad * 1.30;
+                if (conIva < 120) {
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva;
+                }
+                else {
+                    impuesto = conIva * 0.10;
+                    final = conIva + impuesto;
+                    document.getElementById("precioDescuento").value = "El precio final es de $" + conIva + " siendo que pago $" + impuesto + " de IIBB.";
+                }
+            }
+            break;
+        default:
+            conIva = lampara * cantidad * 1.50;
+            if (conIva < 120) {
+                document.getElementById("precioDescuento").value = "El precio final es de $" + conIva;
+            }
+            else {
+                impuesto = conIva * 0.10;
+                final = conIva + impuesto;
+                document.getElementById("precioDescuento").value = "El precio final es de $" + conIva + " siendo que pago $" + impuesto + " de IIBB.";
+            }
+            break;
+    }
 }
